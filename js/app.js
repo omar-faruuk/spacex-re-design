93209a1617f5494b5198a42b74bdc0b41760d207
omar-farukk <omarfaruq.anon@gmail.com>
2021-09-12 23:58:05 +0600

    final commit

--- js/app.js
+++ js/app.js
@@ -5,5 +5,27 @@ social.forEach(function(social,index){
   social.style.animation = `moveIn 0.6s cubic-bezier(.51,.92,.24,1.15) forwards ${index/5}s`;
 })
 
+let rocketPieces = document.querySelectorAll('.rocket-body span');
+let rocket = document.querySelector('.rocket');
+let trigerStart = window.innerHeight / 5;
+
+let rocketOffsetTop = rocket.offsetTop;
+let thirdOffsetTop = rocketPieces[2].offsetTop;
+
+document.addEventListener('scroll', (e) =>{
+  if(window.scrollY > (rocketOffsetTop - trigerStart)){
+    rocketPieces[0].classList.add('active');
+    rocketPieces[1].classList.add('active');
+  }else{
+    rocketPieces[0].classList.remove('active');
+    rocketPieces[1].classList.remove('active');
+  }
+
+  if(window.scrollY > (thirdOffsetTop - trigerStart)){
+    rocketPieces[2].classList.add('active');
+  }else{
+    rocketPieces[2].classList.remove('active');
+  }
+})
 
 }())
